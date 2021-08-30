c12ca40b5efdff8ea392fedf16d9bd2971cdb78b
Ramkumar C <kumarramkumar2000@gmail.com>
2021-08-30 21:14:36 +0530

    Commit

---
+++ src/main/java/com/curriculum/CurriculumManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.curriculum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculumManagementAppApplication.class, args);
+	}
+
+}
